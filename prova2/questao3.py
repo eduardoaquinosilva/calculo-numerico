34c65f90707a6dcda8159caf3d14571cb6bd33e7
Eduardo Aquino da Silva <93106630+eduardoaquinosilva@users.noreply.github.com>
2024-12-13 16:35:01 -0300

    adiciona questao 3 da prova 2

---
+++ prova2/questao3.py
@@ -0,0 +1,43 @@
+from numpy import linalg
+from questao2 import regressive_substitution
+
+
+def gaussian_elimination(coefficient_matrix: list[list[float]], constant_vector: list[float]) -> list[float]:
+    n = len(coefficient_matrix)
+
+    for k in range(0, n - 1):
+        if coefficient_matrix[k][k] == 0:
+            raise UserWarning
+
+        for i in range(k + 1, n):
+            m = coefficient_matrix[i][k] / coefficient_matrix[k][k]
+            coefficient_matrix[i][k] = 0
+
+            for j in range(k + 1, n):
+                coefficient_matrix[i][j] = coefficient_matrix[i][j] - m * coefficient_matrix[k][j]
+            
+            constant_vector[i] = constant_vector[i] - m * constant_vector[k]
+    
+    return regressive_substitution(coefficient_matrix, constant_vector)
+
+
+def main() -> None:
+    A = [[4, -2, -3, 6], [-6, 7, 6.5, -6], [1, 7.5, 6.25, 5.5], [-12, 22, 15.5, -1]]
+    B = [12, -6.5, 16, 17]
+
+    try:
+        if not linalg.det(A):
+            raise ValueError
+
+        x = gaussian_elimination(A, B)
+        
+        for a in range(1, len(x) + 1):
+            print(f'x{a} = {x[a - 1]}')
+    except ValueError:
+        print('O sistema linear não pode ser resolvido, pois a matriz A é singular.')
+    except UserWarning:
+        print('O sistema linear não pode ser resolvido, pois a matriz A possui um pivô nulo.\nRecomenda-se a troca de linhas')
+
+
+if __name__ == '__main__':
+    main()
